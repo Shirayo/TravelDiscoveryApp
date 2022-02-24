754fd1c46d48a1ab462be4b7ddd928426bd9387f
Shirayo <iphoje@gmail.com>
2022-02-24 15:29:27 +0300

    Initial Commit

---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Vasili on 24.02.22.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
